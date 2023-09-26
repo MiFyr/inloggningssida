b372526bccb35304d474bc82cde763b9ccd3c51a
Milla Fyrman <millafyrman@gmail.com>
2023-09-26 16:06:45 +0200

    error message

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//content
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const logInButton = document.getElementById("login-btn");
+const mainTag = document.querySelector("main");
+
+logInButton.addEventListener("click", checkInput);
+
+function checkInput() {
+  if (username.value == "Bella" && password.value == "qwe123") {
+    loggedIn();
+  } else {
+    console.log("Fel uppgifter");
+    let errorDiv = document.createElement("p");
+    let errorText = document.createTextNode("Felaktiga inloggningsuppgifter");
+    errorDiv.appendChild(errorText);
+
+    mainTag.insertAdjacentElement("afterbegin", errorDiv);
+  }
+}
+
+function loggedIn() {
+  let logInCard = document.getElementById("login-card");
+  logInCard.style.display = "none";
+}
